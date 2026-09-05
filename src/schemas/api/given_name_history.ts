f7040f51389d88e8f2eaaa1d5eace1f25ac59cb1
saxomoose <mathieu.tulpinck@gmail.com>
2026-09-05 11:01:27 +0200

    Sync

--- src/schemas/api/given_name_history.ts
+++ src/schemas/api/given_name_history.ts
@@ -2,12 +2,13 @@ import { Type } from "typebox";
 import { GivenName } from "../name";
 import { Attribute, AttributeValue, Entity, Value, } from "../eav";
 import { RegisteredNaturalPerson } from "../domestic_natural_person_register";
-import { EffectivityPeriod } from "../temporal";
+import { DateTime, EffectivityPeriod } from "../temporal";
 
 // Historic view on given name attribute.
 const GivenNameHistory = Type.Object({
     subject: Entity(RegisteredNaturalPerson),
     attribute: Attribute(GivenName),
+    cutOffTime: DateTime,
     history: Type.Array(Value(GivenName))
 });
 
